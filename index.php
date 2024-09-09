2e45eb1e9974b82242da0094d51e819a49fc9af2
okelala123 <daicmngia@gmail.com>
2024-09-09 10:47:55 +0700

    'update'

--- index.php
+++ index.php
@@ -6,3 +6,8 @@ require 'vendor/autoload.php';
 $login = new VifoServiceFactory('dev');
 $a =$login->login('NEWSPACE_sale_demo','newspace@vifo123');
 print_r($a);
+// "require": {
+//     "namshi/cuzzle": "^2.0",
+//     "monolog/monolog": "2.1",
+//     "vlucas/phpdotenv": "^5.6"
+// },
